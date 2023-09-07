b8642fbd9b66cf8f4741ad6dc672be3e05abcf39
Mohammad-Haiderr-Nadeem <142008284+Mohammad-Haiderr-Nadeem@users.noreply.github.com>
2023-09-07 16:29:57 +0500

    Add files via upload

--- Form.js
+++ Form.js
@@ -1,66 +1,37 @@
 import React from "react";
+import InputWithLabel from "./InputWithLabel";
 
 export default function Form(props) {
   return (
     <div>
-      <label htmlFor="Task">
-        <b>Task</b>
-        <br></br>
-        <input
-          type="text"
-          name="task"
-          placeholder="Add Task"
-          required
-          value={props.task}
-          onChange={props.handleOnChangeTask}
-          style={{ width: "95%", padding: "8px" }}
-        ></input>
-      </label>
-      <br></br>
-      <br></br>
-      <label htmlFor="Date">
-        <b>Day</b>
-        <br></br>
-        <input
-          type="date"
-          name="myDate"
-          placeholder="Please select your date.."
-          required
-          value={props.myDate}
-          onChange={props.handleOnChangeDate}
-          style={{ width: "95%", padding: "8px" }}
-        ></input>
-      </label>
-      <br></br>
-      <br></br>
-      <label htmlFor="Time">
-        <b>Time</b>
-        <br></br>
-        <input
-          type="time"
-          name="myDate"
-          placeholder="Add time"
-          required
-          value={props.myTime}
-          onChange={props.handleOnChangeTime}
-          style={{ width: "95%", padding: "8px" }}
-        ></input>
-      </label>
-      <br></br>
-      <br></br>
-      <label htmlFor="description">
-        <b>Description</b>
-        <br></br>
-        <input
-          type="text"
-          name="task"
-          placeholder="Add Description"
-          required
-          value={props.description}
-          onChange={props.handleOnChangeDescription}
-          style={{ width: "95%", padding: "8px" }}
-        ></input>
-      </label>
+      <InputWithLabel
+        title="Task"
+        type="text"
+        value={props.task}
+        onChange={props.handleOnChangeTask}
+        placeholder="Add Task"
+      />
+      <InputWithLabel
+        title="Date"
+        type="date"
+        value={props.myDate}
+        onChange={props.handleOnChangeDate}
+        placeholder="Add Date"
+      />
+      <InputWithLabel
+        title="Time"
+        type="time"
+        value={props.myTime}
+        onChange={props.handleOnChangeTime}
+        placeholder="Add Time"
+      />
+      <InputWithLabel
+        title="Description"
+        type="text"
+        value={props.description}
+        onChange={props.handleOnChangeDescription}
+        placeholder="Add Description"
+      />
     </div>
   );
 }
